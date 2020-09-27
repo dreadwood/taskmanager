3df43e36c802564a03c9e4ab114e715422710d43
srg <skabass@yandex.ru>
2020-09-27 12:43:07 +0300

    7.1.8 Добавляет презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import SiteMenuView from './view/site-menu.js';
-import FilterView from './view/filter.js';
 // import StatisticView from './view/statistic.js';
 import {generateTask} from './mock/task.js';
 import BoardPresenter from './presenter/board.js';
+import FilterPresenter from './presenter/filter.js';
 import TasksModel from './model/tasks.js';
 import FilterModel from './model/filter.js';
 import {render} from './utils/render.js';
@@ -10,25 +10,19 @@ import {render} from './utils/render.js';
 const TASK_COUNT = 22;
 
 const tasks = new Array(TASK_COUNT).fill(``).map(generateTask);
-const filters = [
-  {
-    type: `all`,
-    name: `ALL`,
-    count: 0
-  }
-];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
 
-const filterModel = new FilterModel;
+const filterModel = new FilterModel();
 
 const mainElement = document.querySelector(`.main`);
 const headerElement = mainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(mainElement, tasksModel);
-
 render(headerElement, new SiteMenuView());
-render(mainElement, new FilterView(filters, `all`));
 
+const boardPresenter = new BoardPresenter(mainElement, tasksModel);
+const filterPresenter = new FilterPresenter(mainElement, filterModel, tasksModel);
+
+filterPresenter.init();
 boardPresenter.init();
---
+++ src/utils/filter.js
@@ -0,0 +1,11 @@
+import {FilterType} from '../const.js';
+import {isExpiredTask, isExpiredTaskToday, isRepeatingTask} from './task.js';
+
+export const filter = {
+  [FilterType.ALL]: (tasks) => tasks.filter((task) => !task.isArchive),
+  [FilterType.OVERDUE]: (tasks) => tasks.filter((task) => isExpiredTask(task.dueDate)),
+  [FilterType.TODAY]: (tasks) => tasks.filter((task) => isExpiredTaskToday(task.dueDate)),
+  [FilterType.FAVORITES]: (tasks) => tasks.filter((task) => task.isFavorite),
+  [FilterType.REPEATING]: (tasks) => tasks.filter((task) => isRepeatingTask(task.repeatingDays)),
+  [FilterType.ARCHIVE]: (tasks) => tasks.filter((task) => task.isArchive)
+};
