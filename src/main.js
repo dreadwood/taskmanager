f9fd0dc5cb32ff84712530ad5e59e227c9351d61
srg <skabass@yandex.ru>
2020-09-27 14:18:48 +0300

    7.2.1 Настраиваем меню приложения

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import FilterPresenter from './presenter/filter.js';
 import TasksModel from './model/tasks.js';
 import FilterModel from './model/filter.js';
 import {render} from './utils/render.js';
+import {MenuItem} from './const.js';
 
 const TASK_COUNT = 22;
 
@@ -19,10 +20,32 @@ const filterModel = new FilterModel();
 const mainElement = document.querySelector(`.main`);
 const headerElement = mainElement.querySelector(`.main__control`);
 
-render(headerElement, new SiteMenuView());
+const siteMenuComponent = new SiteMenuView();
+render(headerElement, siteMenuComponent);
 
 const boardPresenter = new BoardPresenter(mainElement, tasksModel, filterModel);
 const filterPresenter = new FilterPresenter(mainElement, filterModel, tasksModel);
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.ADD_NEW_TASK:
+      // Скрыть статистику
+      // Показать доску
+      // Показать форму добавления новой задачи
+      // Убрать выделение с ADD NEW TASK после сохранения
+      break;
+    case MenuItem.TASKS:
+      // Показать доску
+      // Скрыть статистику
+      break;
+    case MenuItem.STATISTICS:
+      // Скрыть доску
+      // Показать статистику
+      break;
+  }
+};
+
+siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
 
 filterPresenter.init();
 boardPresenter.init();
