5ced9497adc24fc170665962a46d5d88a6625bd3
srg <skabass@yandex.ru>
2020-09-27 12:24:06 +0300

    7.1.7 Добавляет модель для фильтра

--- src/main.js
+++ src/main.js
@@ -4,14 +4,24 @@ import FilterView from './view/filter.js';
 import {generateTask} from './mock/task.js';
 import BoardPresenter from './presenter/board.js';
 import TasksModel from './model/tasks.js';
+import FilterModel from './model/filter.js';
 import {render} from './utils/render.js';
 
 const TASK_COUNT = 22;
 
 const tasks = new Array(TASK_COUNT).fill(``).map(generateTask);
+const filters = [
+  {
+    type: `all`,
+    name: `ALL`,
+    count: 0
+  }
+];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
+
+const filterModel = new FilterModel;
 
 const mainElement = document.querySelector(`.main`);
 const headerElement = mainElement.querySelector(`.main__control`);
@@ -19,6 +29,6 @@ const headerElement = mainElement.querySelector(`.main__control`);
 const boardPresenter = new BoardPresenter(mainElement, tasksModel);
 
 render(headerElement, new SiteMenuView());
-render(mainElement, new FilterView(tasks));
+render(mainElement, new FilterView(filters, `all`));
 
 boardPresenter.init();
